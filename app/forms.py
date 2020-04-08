51cc8fe7f9ec6b8c0558ec24d25e811f57487f79
Inkvizzz <Inkvizzzz@gmail.com>
2020-04-08 12:29:49 +0300

    Update forms.py

--- app/forms.py
+++ app/forms.py
@@ -5,26 +5,26 @@ from wtforms.validators import DataRequired, Email, EqualTo, ValidationError
 
 
 class LoginForm(FlaskForm):
-    username = StringField('Username', validators=[DataRequired()])
+    inn = StringField('inn', validators=[DataRequired()])
     password = PasswordField('Password', validators=[DataRequired()])
     remember_me = BooleanField('Remember me')
     submit = SubmitField('Sign in')
 
 
 class RegistrationForm(FlaskForm):
-    username = StringField('Username', validators=[DataRequired()])
+    inn = StringField('inn', validators=[DataRequired()])
     email = StringField('Email', validators= [DataRequired(), Email()])
     password1 = PasswordField('Password', validators=[DataRequired()])
     password2 = PasswordField('Repeat password', validators=[DataRequired(), EqualTo('password1')])
     submit = SubmitField('Register')
 
 def validate_username(self,username):
-    user = Book.query.filter_by(inn=username.data).first()
+    user = Store.query.filter_by(inn=username.data).first()
     if user:
         raise ValidationError('User with that username already exists')
 
 
 def validate_email(self, email):
-    user = Book.query.filter_by(email=email.data).first()
+    user = Store.query.filter_by(email=email.data).first()
     if user:
         raise ValidationError('User with that email already exists')
